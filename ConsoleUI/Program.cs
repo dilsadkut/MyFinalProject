4c2d6ea5199962092d65ec23d6af4fc89da6d13e
Dılşad Kut <61744549+dilsadkut@users.noreply.github.com>
2021-02-12 11:01:55 +0300

    DTO Added

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,15 +11,21 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            // ProductTest();
+            //Data Transformation Object
+             ProductTest();
             //IoC 
+            //CategoryTest();
+
+        }
+
+        private static void CategoryTest()
+        {
             CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
-           
+
             foreach (var category in categoryManager.GetAll())
             {
                 Console.WriteLine(category.CategoryName);
             }
-
         }
 
         private static void ProductTest()
@@ -30,9 +36,9 @@ namespace ConsoleUI
 
 
 
-            foreach (var product in productManager.GetByUnitPrice(40, 100))
+            foreach (var product in productManager.GetProductDetails())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(product.ProductName+ "/" + product.CategoryName);
             }
         }
     }
